43abe6450878bb4b60e3e981a0d18a3950943f3e
Max Pavlov <m.n.pavlov@gmail.com>
2025-12-25 18:57:54 +0500

    Deploy prepare

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/christmas-menu/', // имя репозитория на GitHub
   plugins: [
     react(),
     svgr({
